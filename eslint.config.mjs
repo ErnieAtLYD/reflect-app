bd325867ec045f0f9181eaa99d6c3742d5eac56a
Ernie Hsiung <e@erniehsiung.com>
2025-07-31 13:02:48 -0700

    feat: add TailwindCSS configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,43 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:jsx-a11y/recommended"
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:jsx-a11y/recommended'
   ),
-  ...compat.plugins("import"),
+  ...compat.plugins('import'),
   {
     rules: {
-      "import/order": [
-        "error",
+      'import/order': [
+        'error',
         {
           groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
           ],
-          "newlines-between": "always",
+          'newlines-between': 'always',
           alphabetize: {
-            order: "asc",
+            order: 'asc',
             caseInsensitive: true,
           },
         },
       ],
     },
   },
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
